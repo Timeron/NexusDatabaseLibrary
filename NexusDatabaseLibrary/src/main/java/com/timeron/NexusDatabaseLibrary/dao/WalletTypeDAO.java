898c5f74ead90ad3f61cef32f812af41c42afd3d
Timeron <timeron@gmail.com>
2015-02-05 22:21:43 +0100

    NDL

--- NexusDatabaseLibrary/src/main/java/com/timeron/NexusDatabaseLibrary/dao/WalletTypeDAO.java
+++ NexusDatabaseLibrary/src/main/java/com/timeron/NexusDatabaseLibrary/dao/WalletTypeDAO.java
@@ -25,7 +25,7 @@ public class WalletTypeDAO extends DaoImp<WalletType>{
 		session.beginTransaction();
 		criteria = session.createCriteria(WalletType.class);
 		criteria.setResultTransformer(criteria.DISTINCT_ROOT_ENTITY);
-		criteria.addOrder(Order.asc("date"));
+		criteria.addOrder(Order.asc("timestamp"));
 		result = criteria.list();
 		
 		session.close();
