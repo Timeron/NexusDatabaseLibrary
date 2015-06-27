d7f5b0a754a6d28f337879258c2af3d9fec0c22b
Timeron <timeron@gmail.com>
2015-06-27 10:28:33 +0200

    JTASK-22

--- NexusDatabaseLibrary/src/main/java/com/timeron/NexusDatabaseLibrary/dao/JTaskDAO.java
+++ NexusDatabaseLibrary/src/main/java/com/timeron/NexusDatabaseLibrary/dao/JTaskDAO.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.timeron.NexusDatabaseLibrary.Entity.JProject;
 import com.timeron.NexusDatabaseLibrary.Entity.JTask;
+import com.timeron.NexusDatabaseLibrary.dao.Enum.Direction;
 import com.timeron.NexusDatabaseLibrary.helper.JpaHelper;
 
 @Repository
@@ -35,6 +38,7 @@ public class JTaskDAO extends DaoImp<JTask> {
 		return jTasks;
 	}
 	
+	@Transactional
 	@SuppressWarnings("unchecked")
 	public List<JTask> getByProjectId(int projectId) {
 		LOG.info("enter getByProjectId");
@@ -47,5 +51,59 @@ public class JTaskDAO extends DaoImp<JTask> {
 		LOG.info("exit getByProjectId");
 		return jTasks;
 	}
+
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public List<JTask> getByProjectId(Integer projectId, String orderBy, String direction, int maxResults) {
+		LOG.info("enter getByProjectId");
+		List<JTask> jTasks;
+		Criteria criteria = JpaHelper.createCriteria(entityManager, persistantClass);
+		
+		criteria.add(Restrictions.eq("project", projectId));
+		if(direction != Direction.NONE){
+			if(direction == Direction.ASC){
+				criteria.addOrder(Order.asc(orderBy));
+			}else{
+				criteria.addOrder(Order.desc(orderBy));
+			}
+		}
+		if(maxResults > 0){
+			criteria.setMaxResults(maxResults);
+		}
+		
+		jTasks = (List<JTask>) criteria.list();
+		
+		LOG.info("exit getByProjectId");
+		return jTasks;
+		
+		
+	}
+	
+	@Transactional
+	public String getLastName(JProject project){
+		LOG.info("enter getLastId");
+		JTask entity = null;
+		String lastName = "";
+		try {
+			Criteria criteria = JpaHelper.createCriteria(entityManager, persistantClass);
+			criteria.add(Restrictions.eq("project", project));
+			criteria.addOrder(Order.desc("id"));
+			criteria.setMaxResults(1);
+			if(criteria.list().size() > 0){
+				entity = (JTask) criteria.list().get(0);
+				lastName = entity.getName();
+			}else{
+				lastName = "";
+			}
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+		}finally {
+
+		}
+		LOG.info("exit getLastId");
+		return lastName;
+	}
+	
+
 
 }
