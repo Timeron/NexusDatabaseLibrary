3b03c659c74bc68f912072754ccc1936470c6fd2
Timeron <timeron@gmail.com>
2015-07-24 21:39:43 +0200

    JTASK-39

--- NexusDatabaseLibrary/src/main/java/com/timeron/NexusDatabaseLibrary/dao/JHistoryDAO.java
+++ NexusDatabaseLibrary/src/main/java/com/timeron/NexusDatabaseLibrary/dao/JHistoryDAO.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ public class JHistoryDAO extends DaoImp<JHistory> {
 		List<JHistory> jHistories;
 		Criteria criteria = JpaHelper.createCriteria(entityManager, persistantClass);
 		criteria.add(Restrictions.eq("task", task));
+		criteria.addOrder(Order.desc("created"));
 		
 		if(criteria.list().size() > 0){
 			jHistories = (List<JHistory>) criteria.list();
